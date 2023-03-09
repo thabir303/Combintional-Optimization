75ac3dc5077f9490b57a6f28f7b7fb1299bf7768
thabir303 <109710837+thabir303@users.noreply.github.com>
2023-03-08 22:16:03 -0800

    Add files via upload

---
+++ dfs.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5 + 10;
+vector<int>g[N];
+bool vis[N];
+
+void dfs(int vertex)
+{
+    /* Take action on vertex after entering the vertex */
+    //if(vis[vertex]) return; //this can be written if inside for we haven't written vis child condn
+    cout<<vertex<<endl;
+    vis[vertex]=true;
+    for(int child: g[vertex])
+    {
+        cout<<"parent"<<vertex<<",child"<<child<<endl;
+        if(vis[child]) continue;
+        /* Take action on child before entering the child node */
+        dfs(child);
+        /* Take action on child after existing the child node */
+    }
+    /* Take action on child before existing the vertex */
+}
+
+int main()
+{
+    int vertex,edge;
+    cin>>vertex>>edge;
+    for(int i=0; i<edge; i++)
+    {
+        int v1,v2;
+        cin>>v1>>v2;
+        g[v1].push_back(v2);
+        g[v2].push_back(v1);
+    }
+    dfs(g[0]);
+}
+/*
+6 9
+1 3
+1 5
+3 5
+3 4
+3 6
+3 2
+2 6
+4 6
+5 6
+*/
